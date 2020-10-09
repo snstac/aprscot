2239b4c865dcfe0e66ff56e805feff0855cd3469
Greg Albrecht <github.com@undef.net>
2020-10-08 17:48:00 -0700

    replaced setup_requires with tests_requires

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 import setuptools
 
 __title__ = 'aprscot'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __author__ = 'Greg Albrecht W2GMD <oss@undef.net>'
 __copyright__ = 'Copyright 2020 Greg Albrecht'
 __license__ = 'Apache License, Version 2.0'
@@ -44,7 +44,7 @@ setuptools.setup(
     url='https://github.com/ampledata/aprscot',
     zip_safe=False,
     include_package_data=True,
-    setup_requires=[
+    tests_requires=[
         'coverage >= 3.7.1',
         'httpretty >= 0.8.10',
         'pytest'
