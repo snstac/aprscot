82cd7e81227dec958e0c1a5ca981fb2a06611e6d
Greg Albrecht <github.com@undef.net>
2022-03-07 11:20:02 -0800

    patch bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 import setuptools
 
 __title__ = "aprscot"
-__version__ = "5.0.1"
+__version__ = "5.0.2"
 __author__ = "Greg Albrecht W2GMD <oss@undef.net>"
 __copyright__ = "Copyright 2021 Greg Albrecht"
 __license__ = "Apache License, Version 2.0"
